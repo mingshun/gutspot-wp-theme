021bca4955fb3ab41c9c1da24e72ef2ba11136e7
mingshun <gutspot@qq.com>
2013-08-20 11:41:17 +0000

    Updated copyright info

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
     <p>
       本站内容采用&nbsp;<a href="http://creativecommons.org/licenses/by-sa/3.0/deed.zh" target="_blank" rel="license">知识共享署名-相同方式共享 3.0 未本地化版本许可协议</a>&nbsp;进行许可。<br />
       <span class="inline">自豪地采用&nbsp;<a target="_blank" rel="nofollow" href="http://wordpress.org/">WordPress</a>&nbsp;&nbsp;&nbsp;</span>
-      <span class="inline">主题由&nbsp;<a rel="nofollow" href="[<?php text_obfuscation('mailto:gutspot@qq.com'); ?>]">mingshun</a>&nbsp;基于&nbsp;<a target="_blank" rel="nofollow" href="http://twitter.github.com/bootstrap/">Bootstrap</a>&nbsp;框架&nbsp;(<a target="_blank" rel="nofollow" href="http://bootswatch.com/cosmo/">Bootswatch Cosmo</a>&nbsp;主题)&nbsp;制作&nbsp;&nbsp;&nbsp;</span>
+      <span class="inline">主题由&nbsp;<a rel="nofollow" href="[<?php text_obfuscation('mailto:gutspot@qq.com'); ?>]">mingshun</a>&nbsp;基于&nbsp;<a target="_blank" rel="nofollow" href="http://twitter.github.com/bootstrap/">Bootstrap</a>&nbsp;框架&nbsp;(<a target="_blank" rel="nofollow" href="http://bootswatch.com/2/cosmo/">Bootswatch Cosmo</a>&nbsp;主题)&nbsp;制作&nbsp;&nbsp;&nbsp;</span>
       <span class="inline"><a target="_blank" href="<?php bloginfo('rss2_url'); ?>" title="使用 RSS 2.0 订阅本站点内容">文章&nbsp;<abbr title="Really Simple Syndication">RSS</abbr></a>&nbsp;&nbsp;&nbsp;</span>
       <span class="inline"><a target="_blank" href="<?php bloginfo('comments_rss2_url'); ?>" title="使用 RSS 订阅本站点的所有文章的近期评论">评论&nbsp;<abbr title="Really Simple Syndication">RSS</abbr></a></span>
     </p>
