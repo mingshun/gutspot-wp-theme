bdc301f8295ff4dad31f5d8ebe173b6ba986e466
mingshun <gutspot@qq.com>
2013-08-27 13:13:02 +0000

    Added CCS3 loading animation loop

--- page-github-repos.php
+++ page-github-repos.php
@@ -2,9 +2,10 @@
       <div class="main-content row-fluid">
         <div id="repos" data-github-type="<?php print get_option('gutspot_github_type'); ?>" data-github-login="<?php print get_option('gutspot_github_login'); ?>">
           <div class="row-fluid">
-            <p class="text-center">
-              <i class="icon-refresh icon-spin icon-4x" id="github-loading"></i>
-            </p>
+            <div id="github-loading">
+              <div class="outer-circle"></div>
+              <div class="inner-circle"></div>
+            </div>
           </div>
         </div>
       </div>
